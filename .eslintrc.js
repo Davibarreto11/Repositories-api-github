43b78e0046e2b17a8801a6491cb79a8209cbe5c3
Davibarreto11 <daviarturdesa686@gmail.com>
2023-06-21 14:38:23 -0300

    Configuration project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] },
+    ],
+  },
+};
